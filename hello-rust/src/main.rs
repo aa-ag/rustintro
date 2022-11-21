e43b6cf45aa0489c2cb2c28cebc15c02cedff7a7
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-21 17:50:26 -0600

    Add mutable variable example

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,7 +1,15 @@
-
 fn main() {
-    // Excercise
-    let precision = 3;
-    let pi = 3.141592;
-    println!("{:.1$}", pi, precision);
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    // Ok
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+
+    // Error!
+    // _immutable_binding += 1;
+    // FIXME ^ Comment out this line
 }
