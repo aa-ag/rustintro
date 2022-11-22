ef986114063bb729f6462da466d0ec679b89f695
aa-ag <aaron.aguerrevere@gmail.com>
2022-11-21 18:03:03 -0600

    Add example of overflow substraction

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,15 +1,23 @@
 fn main() {
-    let _immutable_binding = 1;
-    let mut mutable_binding = 1;
+    // Integer addition
+    println!("1 + 2 = {}", 1u32 + 2);
 
-    println!("Before mutation: {}", mutable_binding);
+    // Integer subtraction
+    println!("1 - 2 = {}", 1u32 - 2);
+    // TODO ^ Try changing `1i32` to `1u32` to see why the type is important
 
-    // Ok
-    mutable_binding += 1;
+    // // Short-circuiting boolean logic
+    // println!("true AND false is {}", true && false);
+    // println!("true OR false is {}", true || false);
+    // println!("NOT true is {}", !true);
 
-    println!("After mutation: {}", mutable_binding);
+    // // Bitwise operations
+    // println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    // println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    // println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    // println!("1 << 5 is {}", 1u32 << 5);
+    // println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
 
-    // Error!
-    // _immutable_binding += 1;
-    // FIXME ^ Comment out this line
+    // // Use underscores to improve readability!
+    // println!("One million is written as {}", 1_000_000u32);
 }
