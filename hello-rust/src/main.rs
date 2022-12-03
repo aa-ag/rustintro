c0d7945a8d7f2e3345378261dc1d6d05ab49914d
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-03 00:25:28 -0600

    Add structs to get started

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -6,6 +6,23 @@ struct  Cli {
     state: String,
 }
 
+struct Forecast {
+    timezone: i32,
+    id: i32,
+    weather: Weather,
+}
+
+struct Weather {
+    details: Details
+}
+
+struct  Details {
+    id: i32,
+    main: String,
+    description: String,
+    icon: String,
+}
+
 fn main() {
     let args = Cli::from_args();
     println!("Hello from the city of {}, {}.", args.city, args.state)
