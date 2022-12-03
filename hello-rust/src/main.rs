00227594deeb1e8529d0aa618d3e6cfd4964c898
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-03 00:07:25 -0600

    Add use import

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,18 +1,1 @@
-// An attribute to hide warnings for unused code.
-#![allow(dead_code)]
-
-#[derive(Debug)]
-struct Person {
-    name: String,
-    age: u8,
-}
-
-fn main() {
-    // Create struct with field init shorthand
-    let name = String::from("Peter");
-    let age = 27;
-    let peter = Person { name, age };
-
-    // Print debug struct
-    println!("{:?}", peter);
-}
+use structopt::StructOpt;
