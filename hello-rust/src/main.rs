1ce50e5eef7e4543d040d80026ec3b95d68179c3
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-03 00:10:48 -0600

    Cli taking args

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,1 +1,12 @@
 use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct  Cli {
+    city: String,
+    state: String,
+}
+
+fn main() {
+    let args = Cli::from_args();
+    println!("Hello from the city of {}, {}.", args.city, args.state)
+}
