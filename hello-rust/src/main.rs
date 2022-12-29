63f0c9754989dfca66c6b48b1162b95788b5a62a
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-28 20:24:36 -0500

    Refactor main to go thru expressions

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,19 +1,5 @@
 fn main() {
-    let n = 5;
-    let big_n =
-        if n < 10 && n > -10 {
-            println!(", and is a small number, increase ten-fold");
+    let x = 5u32;
 
-            // This expression returns an `i32`.
-            10 * n
-        } else {
-            println!(", and is a big number, halve the number");
-
-            // This expression must return an `i32` as well.
-            n / 2
-            // TODO ^ Try suppressing this expression with a semicolon.
-        };
-    //   ^ Don't forget to put a semicolon here! All `let` bindings need it.
-
-    println!("{} -> {}", n, big_n);
+    println!("x is {:?}", x);
 }
