c970f8ca689ef4291d163fe60cb21d57d5b93991
aa-ag <aaron.aguerrevere@gmail.com>
2022-12-30 19:12:14 -0600

    Add loop from docs

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,17 +1,26 @@
 fn main() {
-    let x = 5u32;
+    let mut count = 0u32;
 
-    let y = {
-        let x_squared = x * x;
-        let x_cube = x_squared * x;
+    println!("Let's count until infinity!");
 
-        // This expression will be assigned to `y`
-        x_cube + x_squared + x
-    };
+    // Infinite loop
+    loop {
+        count += 1;
 
-    let z = 2 * x;
+        if count == 3 {
+            println!("three");
 
-    println!("x is {:?}", x);
-    println!("y is {:?}", y);
-    println!("z is {:?}", z);
+            // Skip the rest of this iteration
+            continue;
+        }
+
+        println!("{}", count);
+
+        if count == 5 {
+            println!("OK, that's enough");
+
+            // Exit this loop
+            break;
+        }
+    }
 }
