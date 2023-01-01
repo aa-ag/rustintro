b8f0d2dc7bd1eb55bbcf8d98cc62c9558a6ff55f
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-01 02:26:00 -0600

    Add from example

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,10 +1,17 @@
 use std::convert::From;
 
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
 fn main() {
-    let my_str = "hello";
-    println!("{:?}",my_str);
-
-    let my_string = String::from(my_str);
-    println!("{:?}",my_string);
-
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
 }
