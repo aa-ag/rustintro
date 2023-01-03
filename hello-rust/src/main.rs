13174905465d36e7001eaf60de30ccc1942c2788
aa-ag <aaron.aguerrevere@gmail.com>
2023-01-02 18:53:31 -0600

    Add tryfrom with error

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,17 +1,31 @@
-use std::convert::From;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 
-#[derive(Debug)]
-struct Number {
-    value: i32,
-}
+#[derive(Debug, PartialEq)]
+struct EvenNumber(i32);
 
-impl From<i32> for Number {
-    fn from(item: i32) -> Self {
-        Number { value: item }
+impl TryFrom<i32> for EvenNumber {
+    type Error = ();
+
+    fn try_from(value: i32) -> Result<Self, Self::Error> {
+        if value % 2 == 0 {
+            Ok(EvenNumber(value))
+        } else {
+            Err(())
+        }
     }
 }
 
 fn main() {
-    let num = Number::from(30);
-    println!("My number is {:?}", num);
+    // TryFrom
+
+    assert_eq!(EvenNumber::try_from(5), Ok(EvenNumber(8)));
+    assert_eq!(EvenNumber::try_from(5), Err(()));
+
+    // TryInto
+
+    let result: Result<EvenNumber, ()> = 8i32.try_into();
+    assert_eq!(result, Ok(EvenNumber(8)));
+    let result: Result<EvenNumber, ()> = 5i32.try_into();
+    assert_eq!(result, Err(()));
 }
